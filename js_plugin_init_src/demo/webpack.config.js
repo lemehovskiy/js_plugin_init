099a4d98395898eda368479e084720198fd494e9
lemehovskiy <lemehovskiy@gmail.com>
2018-02-12 21:17:51 +0200

    webpack configs

--- js_plugin_init_src/demo/webpack.config.js
+++ js_plugin_init_src/demo/webpack.config.js
@@ -1,15 +1,26 @@
 const autoprefixer = require('autoprefixer');
-
 const NODE_ENV = process.env.NODE_ENV || "development";
 const webpack = require('webpack');
 
 module.exports = {
-    watch: NODE_ENV == 'development',
+
+    watch: true,
+
     entry: ['./index.es6'],
+
     output: {
         filename: './build/app.js'
     },
+
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ],
+
     module: {
+
         rules: [
             {
                 test: /\.es6$/,
@@ -21,50 +32,67 @@ module.exports = {
                     }
                 }
             },
+
             {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: NODE_ENV == 'development'
+                test: /\.s?css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: NODE_ENV == 'development',
+                                minimize: NODE_ENV == 'production'
+                            }
+                        },
+                        {
+                            loader: 'resolve-url-loader',
+                            options: {
+                                sourceMap: NODE_ENV == 'development'
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    autoprefixer({
+                                        browsers: ['last 4 version']
+                                    })
+                                ],
+                                sourceMap: 'inline'
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: NODE_ENV == 'development'
+                            }
                         }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                autoprefixer({
-                                    browsers:['last 4 version']
-                                })
-                            ]
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                        }
-                    }
-                ]
+                    ]
+                })
+
+            },
+
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
+                loader: 'url-loader?limit=100000'
             }
 
         ]
     },
 
-    devtool: NODE_ENV == 'development' ? "source-map" : false,
+    devtool: NODE_ENV == 'development' ? "source-map" : false
 
-    resolve: {
-        alias: {
-            jquery: "jquery/src/jquery"
-        }
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        })
-    ]
 };
+
+
+if (NODE_ENV == 'production') {
+
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    )
+}
